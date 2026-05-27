a761b3f14a63a8a69919d730c3fb2aa895221223
AbrahamAdugna <135274188+xlxuxs@users.noreply.github.com>
2026-05-27 11:51:00 +0300

    Delete backend directory

--- backend/src/routes/smsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const smsController = require("../controllers/smsController");
-const limiters = require("../config/rateLimits");
-
-router.post("/receive", limiters.smsReceive, smsController.receiveSms);
-router.get("/results", smsController.getResults);
-
-module.exports = router;
